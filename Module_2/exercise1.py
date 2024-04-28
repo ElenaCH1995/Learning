7893edc0fc6749b4bc956e5eedec1498cb077c00
d.gorodenko <d.gorodenko@neurone.ru>
2024-04-29 00:38:33 +0300

    работа над ошибками

--- Module_2/exercise1.py
+++ Module_2/exercise1.py
@@ -4,5 +4,13 @@ print('и')
 pet = 'кот'
 print(pet)
 
+# TODO можно переменные задавать в начале файла а потом выводить их в консоль
+# TODO например:
 
+client = 'Петя'
+pet = 'кот'
+
+print(client)
+print('и')
+print(pet)
 
--- Module_2/exercise2.py
+++ Module_2/exercise2.py
@@ -2,3 +2,7 @@ r = 'Red'
 g = 'Green'
 b = 'Blue'
 print(r + ' ' + b + ' ' + g + ' ' + r + g + b + b + g + b)
+
+# TODO тут ошибка в примере ответа вывод выглядит следующим образом: Red Blue Green RedGreenBlue Blue GreenBlue
+# TODO у тебя сейчас Red Blue Green RedGreenBlueBlueGreenBlue
+# TODO сравни и передалай
