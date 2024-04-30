0cb60152504929865f6189bf49849de9a7f91e82
d.gorodenko <d.gorodenko@neurone.ru>
2024-04-30 14:56:00 +0300

    ошибки

--- Module_2/exercise6.py
+++ Module_2/exercise6.py
@@ -1,3 +1,5 @@
 departure = input('Введите город вылета: ')
 arrival = input('Введите город прилета: ')
 print(departure + ' ' + '=' + ' ' + arrival)
+
+# TODO не верный вывод в сравнении с заданием
